5633e235b14ab6252c2d65a4402e47e5309a8511
Nisha Dhungana <nishadhungana.080@kathford.edu.np>
2025-12-05 12:12:34 +0545

    logo updated

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='p-5 text-center bg-[#9E8582]'>
-      <div className=''>
-
-      </div>
       &copy; Copyright 2025.
     </div>
+    
   )
 }
 
