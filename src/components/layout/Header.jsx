3d8c895da6ec3c071e891271fa0325c489867073
Nisha Dhungana <nishadhungana.080@kathford.edu.np>
2025-12-07 14:49:41 +0545

    header layout updated

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     <>
       <header className='bg-[#4B2E2B] text-amber-50'>
         <nav className='md:grid grid-cols-4 '>
-          <div className='col-span-2 flex gap-5 items-center'>
+          <div className='col-span-2 flex gap-3 items-center'>
           <Link to={'/'}><img src={Logo} alt="cafeLogo" className='w-13 h-auto rounded-full px-2 py-2' /></Link>
             <Link to={'/'}> <h1 className='font-bold text-2xl col-span-1 text-center hover:cursor-pointer flex items-center justify-center'>Sips</h1> </Link>
          </div>
